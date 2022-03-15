f6375d293b1c35e5cbf3b031c3ae1917004ed324
Arvin <Arvin@Ravanth>
2022-03-15 14:57:11 +0530

    2nd commit

--- src/test/java/launch/JenkinsTest.java
+++ src/test/java/launch/JenkinsTest.java
@@ -4,25 +4,25 @@ import org.testng.annotations.Test;
 
 public class JenkinsTest 
 {
-	@Test
-	public void demo()
-	{
-		System.out.println("User-1 first commit");
-		System.out.println("hiii");
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("dell");
-		System.out.println("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");
-		System.out.println("dell");
-		System.out.println("hello");
-		System.out.println("dell");
-		System.out.println("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");
-		System.out.println("dell");
-
-
+			@Test
+			public void demo()
+			{
+				System.out.println("User-1 first commit");
+				System.out.println("hiii");
+				System.out.println("hello");
+				System.out.println("hello");
+				System.out.println("hello");
+				System.out.println("dell");
+				System.out.println("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");
+				System.out.println("dell");
+				System.out.println("hello");
+				System.out.println("dell");
+				System.out.println("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");
+				System.out.println("dell");
 		
 		
-	}
+				
+				
+			}
 
 }
---
+++ src/test/java/launch/SimpleProgramTest.java
@@ -0,0 +1,17 @@
+package launch;
+
+import org.testng.annotations.Test;
+
+public class SimpleProgramTest {
+	@Test
+	public void demo()
+	{
+		System.out.println("User-1 first commit");
+		System.out.println("hiii");
+		
+		System.out.println("chandanshreekanth03");
+		System.out.println("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");
+		
+		
+	}
+}
