68402d9c623795a4b84ca4e91157dc954c027e4f
Arvin <Arvin@Ravanth>
2022-03-15 13:26:41 +0530

    first

---
+++ src/test/java/launch/JenkinsTest.java
@@ -0,0 +1,21 @@
+package launch;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest 
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("User-1 first commit");
+		System.out.println("hiii");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("dell");
+		System.out.println("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");
+		
+		
+	}
+
+}
